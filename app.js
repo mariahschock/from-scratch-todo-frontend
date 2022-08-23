c5e34d6543cc86de65c41f9d498467831125a495
Mariah <mariahschock@gmail.com>
2022-08-23 10:28:06 -0700

    created functionality for sign up form

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
-
+const signUpForm = document.getElementById('sign-up');
 // let state
-
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(signUpForm);
+    await signUpUser({ 
+        email: formData.get('email'),
+        password: formData.get('password'),
+        firstName: formData.get('first-name'),
+        lastName: formData.get('last-name'),
+    });
+  
+});
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+redirectIfLoggedIn();
